c6356e6a4be8323c7f42993e3c4141e31461e52d
rollinavocado <149028082+rollinavocado@users.noreply.github.com>
2024-02-06 00:26:36 +0900

    수정

--- my-app/App.js
+++ my-app/App.js
@@ -1,23 +1,15 @@
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <WebView
       style={styles.container}
-      source={{ uri: 'https://naver.com' }}
+      source={{ uri: 'http://cigar8.org' }}
     />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
-});
 // import { StyleSheet, Text, View } from 'react-native';
 
 // export default function App() {
